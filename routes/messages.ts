89f4b69512a57d4d8b60b3724c9a7b2e89338421
Klimova Alina <alinaklimova82@gmail.com>
2024-08-06 02:50:40 +0600

    Added logic to create a messages directory if it doesn't exist.

--- routes/messages.ts
+++ routes/messages.ts
@@ -6,19 +6,24 @@ const messagesRouter = express.Router();
 const path = './messages';
 
 messagesRouter.post('/', async (req, res) => {
-  const message = req.body.message;
-
-  if (!message) {
-    return res.sendStatus(404);
-  }
-
-  const datetime = new Date().toISOString();
-  const filename = `${path}/${datetime}.txt`;
-  const savedMessage: IMessage = { message, date: datetime };
-
   try {
-    await fs.writeFile(filename, JSON.stringify(savedMessage));
-    res.send(savedMessage);
+    const message = req.body.message;
+    await fs.mkdir(path, { recursive: true });
+
+    if (!message) {
+      return res.sendStatus(404);
+    }
+
+    const datetime = new Date().toISOString();
+    const filename = `${path}/${datetime}.txt`;
+    const savedMessage: IMessage = { message, date: datetime };
+
+    try {
+      await fs.writeFile(filename, JSON.stringify(savedMessage));
+      res.send(savedMessage);
+    } catch (e) {
+      console.error(e);
+    }
   } catch (e) {
     console.error(e);
   }
